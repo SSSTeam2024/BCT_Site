3bb5bff31cbb7c1be98c94e064b144a23749b043
SSSTeam2024 <sss.team2024@gmail.com>
2025-02-07 16:28:47 +0100

    adding offer services

--- assets/js/offreServicesComponent.js
+++ assets/js/offreServicesComponent.js
@@ -52,49 +52,33 @@ function updateOffreServices(offreServicesData) {
   rowSection.classList.add("row");
 
   specificOffreServices.map((offre) => {
-    if (offre.littleTitle.display === "1") {
-      const littleTitle = document.createElement("span");
-      littleTitle.textContent = offre.littleTitle.name;
-      sectionTitle.appendChild(littleTitle);
-    }
-    if (offre.bigTitle.display === "1") {
-      const bigTitle = document.createElement("h2");
-      bigTitle.textContent = offre.bigTitle.name;
-      sectionTitle.appendChild(bigTitle);
-    }
-    offre.cards.map((card, index) => {
-      const imagePath = card.image.startsWith("http")
-        ? card.image
-        : `${baseUrl}${card.image}`;
-      console.log("image", card.image);
-      if (card.display === "1") {
-        const singleService = document.createElement("div");
-        singleService.classList.add("single-service");
-        const uniqueClass = `single-service`;
-        singleService.classList.add(uniqueClass);
+    if (offre.display === "0") {
+      offreContainer.innerHTML = "";
+    } else {
+      if (offre.littleTitle.display === "1") {
+        const littleTitle = document.createElement("span");
+        littleTitle.textContent = offre.littleTitle.name;
+        sectionTitle.appendChild(littleTitle);
+      }
+      if (offre.bigTitle.display === "1") {
+        const bigTitle = document.createElement("h2");
+        bigTitle.textContent = offre.bigTitle.name;
+        sectionTitle.appendChild(bigTitle);
+      }
+      offre.cards.map((card, index) => {
+        const imagePath = card.image.startsWith("http")
+          ? card.image
+          : `${baseUrl}${card.image}`;
+        console.log("image", card.image);
+        if (card.display === "1") {
+          const singleService = document.createElement("div");
+          singleService.classList.add("single-service");
+          const uniqueClass = `single-service`;
+          singleService.classList.add(uniqueClass);
 
-        const style = document.createElement("style");
-        style.textContent = `
-  .${uniqueClass}::before {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-image: url(${imagePath});
-    background-position: center center;
-    background-size: cover;
-    background-repeat: no-repeat;
-    z-index: -1;
-  }
-`;
-        const colSection = document.createElement("div");
-        colSection.classList.add("col-lg-4", "col-sm-6");
-
-        const styleCol = document.createElement("style");
-        styleCol.textContent = `
-    .col-lg-4:nth-child(${index + 1}) .single-service::before {
+          const style = document.createElement("style");
+          style.textContent = `
+    .${uniqueClass}::before {
       content: "";
       position: absolute;
       top: 0;
@@ -108,27 +92,47 @@ function updateOffreServices(offreServicesData) {
       z-index: -1;
     }
   `;
-        document.head.appendChild(style);
-        document.head.appendChild(styleCol);
+          const colSection = document.createElement("div");
+          colSection.classList.add("col-lg-4", "col-sm-6");
 
-        const serviceContentWrap = document.createElement("div");
-        serviceContentWrap.classList.add("service-content-wrap");
-        const iconService = document.createElement("i");
-        card.icon.split(" ").forEach((className) => {
-          iconService.classList.add("icon", className);
-        });
-        const cardTitle = document.createElement("h3");
-        cardTitle.textContent = card.title;
-        const cardContent = document.createElement("p");
-        cardContent.textContent = card.content;
-        serviceContentWrap.appendChild(iconService);
-        serviceContentWrap.appendChild(cardTitle);
-        serviceContentWrap.appendChild(cardContent);
-        singleService.appendChild(serviceContentWrap);
-        colSection.appendChild(singleService);
-        rowSection.appendChild(colSection);
+          const styleCol = document.createElement("style");
+          styleCol.textContent = `
+      .col-lg-4:nth-child(${index + 1}) .single-service::before {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-image: url(${imagePath});
+        background-position: center center;
+        background-size: cover;
+        background-repeat: no-repeat;
+        z-index: -1;
       }
-    });
+    `;
+          document.head.appendChild(style);
+          document.head.appendChild(styleCol);
+
+          const serviceContentWrap = document.createElement("div");
+          serviceContentWrap.classList.add("service-content-wrap");
+          const iconService = document.createElement("i");
+          card.icon.split(" ").forEach((className) => {
+            iconService.classList.add("icon", className);
+          });
+          const cardTitle = document.createElement("h3");
+          cardTitle.textContent = card.title;
+          const cardContent = document.createElement("p");
+          cardContent.textContent = card.content;
+          serviceContentWrap.appendChild(iconService);
+          serviceContentWrap.appendChild(cardTitle);
+          serviceContentWrap.appendChild(cardContent);
+          singleService.appendChild(serviceContentWrap);
+          colSection.appendChild(singleService);
+          rowSection.appendChild(colSection);
+        }
+      });
+    }
   });
   offreContainer.appendChild(sectionTitle);
   offreContainer.appendChild(rowSection);
